a3f3d336f3c359ae210c3a82bdd4959fc168a65c
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 19:27:28 +0200

    JSON REST

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-const students = [
-    {
-        name: 'Paul',
-        age: 15,
-        scores: [
-            {matiere: 'Maths', note: 10},
-            {matiere: 'Français', note: 12},
-            {matiere: 'Anglais', note: 14},
-        ]
-    },
-    {
-        name: 'Marie',
-        age: 14,
-        scores: [
-            {matiere: 'Maths', note: 15},
-            {matiere: 'Français', note: 9},
-            {matiere: 'Anglais', note: 10},
-        ]
-    },
-]
-const json = JSON.stringify(students);  //Convertit la donnée JS au format JSON
-
-console.log(json);
-
-const students2 = JSON.parse(json); //Convertit la donnée JSON au format JS
-
-console.log(students2[0].name); //Cible la valeur de la propriété name du premier élément de l'objet students2
+fetch('https://www.data.gouv.fr/api/1/discussions/?sort=-created&page=1&page_size=20')
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log(`Il existe ${response.total} demandes`)
+        console.log(`Le titre de la première demande est ${response.date[0].title}`)
+    })
+    .catch(error => {
+        console.log(`Erreur ${error}`)
+    })
